17b3db95e0494a3d6e9b9dad9ce6c80fb003f886
Herschel Schwartz <herschelrschwartz@gmail.com>
2023-02-19 07:33:33 -0800

    headroom staging area

--- _site/assets/js/toc-sidebar.js
+++ _site/assets/js/toc-sidebar.js
@@ -1,8 +1,45 @@
+// const sectionTitles = [];
+// document.querySelectorAll("section").forEach(item => sectionTitles.push(item));
+// const tocList = []
+// document.querySelectorAll(".toc a").forEach(item => tocList.push(item));
+
+
+// // select the first to start with, even if it's not intersecting the root margin
+// tocList[0].firstChild.classList.add("toc-active");
+
+// const observer = new IntersectionObserver(entries => {
+//   const entryFilter = entries.filter(entry => entry.isIntersecting);
+//   if (entryFilter.length > 0) {
+//     const topItem = entryFilter[entryFilter.length - 1].target
+//     topItemIndex = tocList.findIndex(item => {
+//       return item.href.includes(topItem.id);
+//     });
+//     const topTocItem = tocList[topItemIndex];
+//     console.log(topTocItem)
+
+//     const aboveTopItem = tocList.slice(0 , topItemIndex);
+//     const belowTopItem = tocList.slice(topItemIndex + 1);
+
+
+//     aboveTopItem.forEach(item => {
+//       // item.classList.add("past-scroll");
+//       item.firstChild.classList.remove("toc-active");
+//     })
+//     // topTocItem.classList.remove("past-scroll");
+//     topTocItem.firstChild.classList.add("toc-active");
+//     belowTopItem.forEach(item => {
+//       item.firstChild.classList.remove("toc-active");
+//       // item.classList.remove("past-scroll");
+//   })
+//   }
+// }, { rootMargin: "-10% 0px -85% 0px" })
+
+// sectionTitles.forEach(title => observer.observe(title));
+
 const sectionTitles = [];
 document.querySelectorAll("section").forEach(item => sectionTitles.push(item));
 const tocList = []
-document.querySelectorAll(".toc a").forEach(item => tocList.push(item));
-
+document.querySelectorAll(".nav-list-main a").forEach(item => tocList.push(item));
 
 // select the first to start with, even if it's not intersecting the root margin
 tocList[0].firstChild.classList.add("toc-active");
@@ -11,17 +48,15 @@ const observer = new IntersectionObserver(entries => {
   const entryFilter = entries.filter(entry => entry.isIntersecting);
   if (entryFilter.length > 0) {
     const topItem = entryFilter[entryFilter.length - 1].target
-console.log("fpoo")
+    
     topItemIndex = tocList.findIndex(item => {
       return item.href.includes(topItem.id);
     });
     const topTocItem = tocList[topItemIndex];
-    console.log(topTocItem)
-
+    
     const aboveTopItem = tocList.slice(0 , topItemIndex);
     const belowTopItem = tocList.slice(topItemIndex + 1);
-
-
+    
     aboveTopItem.forEach(item => {
       // item.classList.add("past-scroll");
       item.firstChild.classList.remove("toc-active");
@@ -32,7 +67,7 @@ console.log("fpoo")
       item.firstChild.classList.remove("toc-active");
       // item.classList.remove("past-scroll");
   })
-  }
+}
 }, { rootMargin: "-10% 0px -85% 0px" })
 
 sectionTitles.forEach(title => observer.observe(title));
